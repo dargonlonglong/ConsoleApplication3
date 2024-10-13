43742f6a3b9593a066fd23967b52d781e0fe79d3
c0958 <c0958@ASUSINTEL15>
2024-10-13 23:38:01 +0800

    加入專案檔案。

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include "MultiplicationTable.h"
+
+// 構造函數與析構函數實作
+MultiplicationTable::MultiplicationTable() {}
+MultiplicationTable::~MultiplicationTable() {}
+
+// 公共成員函數實作
+void MultiplicationTable::printTable() {
+    for (int i = 1; i <= 9; ++i) {
+        for (int j = 1; j <= 9; ++j) {
+            std::cout << i << " * " << j << " = " << i * j << "\t";
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main() {
+    MultiplicationTable table;
+    table.printTable();
+    return 0;
+}
---
+++ ConsoleApplication3/MutiplicationTable.h
@@ -0,0 +1,14 @@
+#ifndef MULTIPLICATIONTABLE_H
+#define MULTIPLICATIONTABLE_H
+
+class MultiplicationTable {
+public:
+    // 構造函數與析構函數
+    MultiplicationTable();
+    ~MultiplicationTable();
+
+    // 公共成員函數
+    void printTable();
+};
+
+#endif // MULTIPLICATIONTABLE_H
